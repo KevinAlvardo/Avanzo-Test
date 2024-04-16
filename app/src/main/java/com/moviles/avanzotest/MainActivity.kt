01dde98e63020b3a959606fe04da3e359f885707
gerardo <UFG@CDV02.ufg.local>
2024-04-16 16:03:56 -0600

    minuto 35

--- app/src/main/java/com/moviles/avanzotest/MainActivity.kt
+++ app/src/main/java/com/moviles/avanzotest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.moviles.avanzotest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+import com.moviles.avanzotest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val window:Window=this@MainActivity.window
+        window.statusBarColor=ContextCompat.getColor(this@MainActivity,R.color.grey)
     }
 }
